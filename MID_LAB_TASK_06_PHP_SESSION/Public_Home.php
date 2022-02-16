b8f369a5ec09b0d19a22927ec8cf2d2cb3313d2a
Mahdi-Abdullah <20-43084-1@student.aiub.edu>
2022-02-16 11:47:19 +0600

    new file added

---
+++ MID_LAB_TASK_06_PHP_SESSION/Public_Home.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Public Home</title>
+</head>
+<body>
+	<p> <a href="Public_Home.php">Home</a> | <a href="login.php">Login</a> | <a href="Registration.php"> Registration</a></p>
+	<p><b> Welcome to xCompany</b></p>
+	<p> Copyright</p>
+</body>
+</html>
